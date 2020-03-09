ee24e83eb41e82b178159602ea51178011ffba6b
Bogdan Petcas <petcas.bogdan@gmail.com>
2020-03-09 22:50:36 +0200

    reached a stable version

--- app/src/components/home/Home.jsx
+++ app/src/components/home/Home.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { showToast } from 'actions/toast';
 
 class Home extends React.Component {
+  componentDidMount() {
+    this.props.showToast({
+      message: 'Welcome!'
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -8,8 +17,8 @@ class Home extends React.Component {
         <h3>Features</h3>
         <p>- .scss support</p>
         <p>- routing</p>
-        <p>- redux (with example)</p>
-        <p>- bundling using webpack</p>
+        <p>- redux</p>
+        <p>- webpack bundling</p>
         <p>- watch mode</p>
         <p>- ability to serve the output folder using http-server</p>
       </div>
@@ -17,4 +26,12 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => {
+  return { location: state.router.location }
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ showToast }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
